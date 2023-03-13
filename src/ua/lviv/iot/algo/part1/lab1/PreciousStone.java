d66a70b96adeaf60d86f0c82ae724d2276baab15
Rostyslav Troian <rostyslav.troian.ir.2022@lpnu.ua>
2023-03-13 16:42:45 +0200

    update lab1

--- src/ua/lviv/iot/algo/part1/lab1/PreciousStone.java
+++ src/ua/lviv/iot/algo/part1/lab1/PreciousStone.java
@@ -1,62 +1,51 @@
 package ua.lviv.iot.algo.part1.lab1;
-import lombok.Setter;
-import lombok.Getter;
-import lombok.ToString;
+
+import lombok.*;
+
 @Setter
 @Getter
 @ToString
-
-public class PreciousStone{
-    private  String name;
+@NoArgsConstructor
+@AllArgsConstructor
+public class PreciousStone {
+    private String name;
     private int carat;
     private String color;
     private int clarity;
     private int pricePerCarat;
-    public PreciousStone(){};
 
-   public PreciousStone (String name, int carat, String color, int clarity, int pricePerCarat){
-        this.name = name;
-        this.carat = carat;
-        this.color = color;
-        this.clarity = clarity;
-        this.pricePerCarat = pricePerCarat;
-    }
-    private static PreciousStone
-    defaultPreciousStone = new PreciousStone();
+    private static PreciousStone defaultPreciousStone = new PreciousStone();
 
-    public static PreciousStone
-    getInstance(){
+    public static PreciousStone getInstance() {
         return defaultPreciousStone;
     }
-    public int getTotalPrice(int pricePerCarat, int carat){
+
+    public int getTotalPrice(int pricePerCarat, int carat) {
         this.pricePerCarat = pricePerCarat;
         this.carat = carat;
         return (pricePerCarat * carat);
     }
-    public int increaseClarity(int clarity){
+
+    public int increaseClarity(int clarity) {
         this.clarity = clarity;
         return (clarity + 1);
     }
-    public void increasePrice(double percentage){
-        pricePerCarat *= (1 + percentage/100);
+
+    public void increasePrice(double percentage) {
+        pricePerCarat *= (1 + percentage / 100);
     }
-    @Override
-    public String toString(){
-        return "PreciousStone {" + "name " + name + ", carat " + carat + '\'' +
-                ", has color " + color + '\'' + " and clarity " + '\''
-               + "his price per carat " + pricePerCarat + '}';
 
-        }
-        public static void main(String[] args) {
-        PreciousStone Stone1 = new PreciousStone();
-        PreciousStone Stone2 = new PreciousStone("Diamond", 50, "green", 3, 150);
-        PreciousStone Stone3 = PreciousStone.getInstance();
-        PreciousStone Stone4 = PreciousStone.getInstance();
+    public static void main(String[] args) {
 
-        PreciousStone[] Stones = {Stone1, Stone2, Stone3, Stone4};
+        PreciousStone[] stones = {
+                new PreciousStone(),
+                new PreciousStone("Diamond", 50, "green", 3, 150),
+                PreciousStone.getInstance(),
+                PreciousStone.getInstance()
+        };
 
-        for(PreciousStone Stone : Stones){
-            System.out.println(Stone.toString());
+        for (var stone : stones) {
+            System.out.println(stone);
         }
     }
 }
